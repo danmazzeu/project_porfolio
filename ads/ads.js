c91aa7dd81b7ffca82c3d9ea5798859a9c514375
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-08 21:00:25 -0300

    upload

---
+++ ads/ads.js
@@ -0,0 +1,30 @@
+const URLS = {
+    "digio": "https://mgm.digio.com.br/bc50/y3ygie6o",
+    "iphone": "https://www.exemplo.com/pagina2",
+};
+
+function getParametroUrl(nome) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(nome);
+}
+
+const adsValor = getParametroUrl("ads");
+const counterSpan = document.getElementById("counter");
+let counter = 3;
+
+if (adsValor && URLS.hasOwnProperty(adsValor)) {
+    counterSpan.textContent = counter;
+
+    const intervalo = setInterval(() => {
+        counter--;
+        counterSpan.textContent = counter;
+
+        if (counter <= 0) {
+            clearInterval(intervalo);
+            window.location.assign(URLS[adsValor]);
+        }
+    }, 1000);
+} else {
+    console.error("URL não encontrada ou parâmetro ads ausente.");
+
+}
