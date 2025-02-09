7561058896a34647fcbf5d3b28cc52fefb30f98d
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-09 04:03:10 -0300

    upload

--- scripts/components/ads.js
+++ scripts/components/ads.js
@@ -1,11 +1,40 @@
+function sendmail(name) {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString('pt-BR');
+    const formattedTime = now.toLocaleTimeString('pt-BR');
+
+    fetch('https://projectsendmail-production.up.railway.app/sendmail', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            from: "danmazzeu9@gmail.com",
+            to: "danmazzeu9@gmail.com",
+            subject: "Novo click anúncio " + name,
+            message: `Horário: ${formattedTime} Data: ${formattedDate}`,
+            smtp: "smtp.gmail.com",
+            port: 465,
+            tls: true,
+            username: "danmazzeu9@gmail.com",
+            password: "fpzj cadg ztit ejmz"
+        })
+    }).then(response => response.json()).then(data => {
+        console.log('Resposta da API:', data);
+    }).catch(error => {
+        console.error('Erro na requisição:', error);
+    });
+}
+    
 const URLS = {
     "digio": "https://mgm.digio.com.br/bc50/y3ygie6o",
     "shopee": "https://s.shopee.com.br/8pWnMHi6Nc",
 };
 
-function getParametroUrl(nome) {
+function getParametroUrl(name) {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(nome);
+    sendmail(name);
+    return urlParams.get(name);
 }
 
 const adsValor = getParametroUrl("ads");
